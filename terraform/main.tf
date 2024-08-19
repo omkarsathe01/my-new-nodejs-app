e2c767adb5d71bd5a8889d41a0ec62c6696422d9
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-08-19 17:50:17 +0530

    Correct Route Table configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,7 +48,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
