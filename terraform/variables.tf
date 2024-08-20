4ac48f459c713e7ddb94c8000c984ccb75d1be24
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-08-20 18:34:07 +0530

    Revert "Change"
    This reverts commit 6f5562d68bf100136642fe2730be48e8172b9b14.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,40 @@ variable "bucket" {
   default = "terraform-practice-bucket-0"
 }
 
-variable "app_count" {
-  type    = number
-  default = 1
+variable "ami" {
+  type        = string
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0ae8f15ae66fe8cda"
+
+  validation {
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "Please provide a valid value for variable AMI."
+  }
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+  sensitive   = true
+}
+
+variable "tags" {
+  type = object({
+    Name = string
+  })
+  description = "Tags for the EC2 instance"
+  default = {
+    Name = "ubuntu"
+  }
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  type    = string
+  default = "10.0.0.0/24"
 }
